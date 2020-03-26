e7df0163e4569294170cb603fa8e0c007601950a
SheebaJ <ashasheebajnn1996@gmail.com>
2020-03-26 18:07:28 +0530

    potfolio feature

--- app/controllers/portfoliios_controller.rb
+++ app/controllers/portfoliios_controller.rb
@@ -2,5 +2,17 @@ class PortfoliiosController < ApplicationController
   def index
       @portfolio_items = Portfoliio.all
   end
-
+  def new
+    @portfolio_item = Portfoliio.new
+  end
+  def create
+    @portfolio_item = Portfoliio.new(params.require(:portfoliio).permit(:title, :subtitle, :body))  
+    respond_to do |format|
+      if @portfolio_item.save
+          format.html {redirect_to portfoliios_path, notice: "added"}
+      else
+          format.html{render :new}  
+      end
+    end
+end
 end
