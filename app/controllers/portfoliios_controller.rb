e38d6917a092bf4639c6ab530565dd1c2bdac32b
SheebaJ <ashasheebajnn1996@gmail.com>
2020-03-26 19:03:18 +0530

    Show

--- app/controllers/portfoliios_controller.rb
+++ app/controllers/portfoliios_controller.rb
@@ -14,5 +14,21 @@ class PortfoliiosController < ApplicationController
           format.html{render :new}  
       end
     end
-end
+  end
+  def edit
+    @portfolio_item = Portfoliio.find(params[:id])
+  end
+  def update
+    @portfolio_item = Portfoliio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfoliio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfoliios_path, notice: 'portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  def show
+    @portfolio_item = Portfoliio.find(params[:id])
+  end
 end
